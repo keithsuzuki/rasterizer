8f4592119f10893e0d38c8c8ab2de0a2cccdbacf
keithsuzuki <keithsuzuki@gmail.com>
2017-07-17 14:24:05 +0900

    C:\Users is now mountes as /users

--- buildpcl.sh
+++ buildpcl.sh
@@ -4,4 +4,4 @@ echo -e "\n### build image with name of rasterizer ###"
 docker build -t keithsuzuki/rasterizer .
 
 echo -e "\n###run container with name of pcl in background###"
-docker run -d --name pcl -t keithsuzuki/rasterizer
+docker run -d --name pcl -t -v //c/Users:/users keithsuzuki/rasterizer
